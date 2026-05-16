bae3ff7cf999ebdcaad640f91c5a33ad27e436fd
jjeancarlos <jeanpastebin@gmail.com>
2026-05-16 16:08:20 -0300

    fix: ajusta alguns erros da tela de login, assim como ajusta as notificações passadas pelo usuário

--- test/system/registration_flow_test.rb
+++ test/system/registration_flow_test.rb
@@ -13,9 +13,12 @@ class RegistrationFlowTest < ApplicationSystemTestCase
     click_on "Continuar"
 
     assert_text "Como você aprende?"
+    assert_selector "#start-button-container.hidden", visible: false
+    assert_button "Começar", disabled: true, visible: false
 
-    choose "Tenho os dois"
+    find("label[for='user_perfil_acessibilidade_ambos']").click
 
+    assert_no_selector "#start-button-container.hidden", visible: false
     assert_button "Começar", disabled: false
     click_on "Começar"
 
