dd624a9a090b65d7a9017277cd66e58beac1238b
jjeancarlos <jeanpastebin@gmail.com>
2026-05-17 16:37:23 -0300

    fix/refactor: refatora fonte, gráficos e corrige erros em alerts

--- app/controllers/aluno/dashboard_controller.rb
+++ app/controllers/aluno/dashboard_controller.rb
@@ -1,16 +1,27 @@
 class Aluno::DashboardController < ApplicationController
   def show
-    @tentativas_semana         = Current.user.tentativas.da_semana
+    @tentativas_semana = Current.user.tentativas.da_semana
     @tentativas_semana_passada = Current.user.tentativas.semana_passada
 
     @xp_por_dia = Current.user.tentativas
-                               .da_semana
-                               .group_by_day(:concluida_em)
-                               .sum(:xp_ganho)
+                              .da_semana
+                              .group_by_day(:concluida_em)
+                              .sum(:xp_ganho)
 
     @missoes_por_tipo = Current.user.tentativas
-                                     .da_semana
-                                     .group(:tipo_missao)
-                                     .count
+                                    .da_semana
+                                    .group(:tipo_missao)
+                                    .count
+
+    range_start = 3.weeks.ago.beginning_of_week
+    range_end = Time.zone.today.end_of_week
+    weekly_counts = Current.user.tentativas
+                              .where(concluida_em: range_start..range_end)
+                              .group_by_week(:concluida_em, range: range_start..range_end)
+                              .count
+
+    @missoes_por_semana = weekly_counts.each_with_index.to_h do |(_, total), index|
+      ["Semana #{index + 1}", total]
+    end
   end
 end
