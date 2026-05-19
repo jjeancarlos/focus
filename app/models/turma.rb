26d16141177c957c01f7f5ed4a6de4c2aba37641
jjeancarlos <jeanpastebin@gmail.com>
2026-05-19 17:36:02 -0300

    feat: adiciona função para pesquisar turmas/alunos ao dashboard do professor

--- app/models/turma.rb
+++ app/models/turma.rb
@@ -2,6 +2,16 @@ class Turma < ApplicationRecord
   belongs_to :professor, class_name: "User", foreign_key: :professor_id
   has_many :alunos, class_name: "User", foreign_key: :turma_id
 
+  scope :do_professor, ->(professor) { where(professor_id: professor.id) }
+  scope :buscar_por_nome_ou_aluno, ->(termo) do
+    sanitized_term = sanitize_sql_like(termo.to_s.strip)
+    next all if sanitized_term.blank?
+
+    left_joins(:alunos)
+      .where("turmas.nome ILIKE :query OR users.name ILIKE :query", query: "%#{sanitized_term}%")
+      .distinct
+  end
+
   before_create :gerar_invite_token
 
   def self.find_by_invite_token(token)
